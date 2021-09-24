3cfa9bfed6c734575b3df03ef71c498f10d74562
Abhijit Shete <abhijitshete13@gmail.com>
2021-09-24 14:38:42 +0530

    Section_4 : 40 Two Vieews for CRUDL

--- cfeapi/status/api/urls.py
+++ cfeapi/status/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import StatusAPIView
+from .views import StatusAPIView, StatusAPIDetailView
 
 urlpatterns = [
     path('',StatusAPIView.as_view()),
+    path('<int:id>/',StatusAPIDetailView.as_view()),
 ]
